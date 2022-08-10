96a6efcc124ebd4b2a108f12e3d61695acaf25b4
Warren Hong <hwhong1129@gmail.com>
2022-08-09 21:45:39 -0700

    init

---
+++ src/components/marquee/marquee.tsx
@@ -0,0 +1,26 @@
+import React from "react";
+import styles from "./marquee.module.css";
+
+export function Marquee() {
+  return (
+    <div className={styles.root}>
+      <ul className={styles.contentWrapper}>
+        <li>1</li>
+        <li>2</li>
+        <li>3</li>
+        <li>4</li>
+        <li>5</li>
+        <li>6</li>
+      </ul>
+
+      <ul aria-hidden="true" className={styles.contentWrapper}>
+        <li>1</li>
+        <li>2</li>
+        <li>3</li>
+        <li>4</li>
+        <li>5</li>
+        <li>6</li>
+      </ul>
+    </div>
+  );
+}
