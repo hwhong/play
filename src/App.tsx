0420e175f8181cc195b65d6c5c6dbe1cc9393f5f
Warren Hong <hwhong1129@gmail.com>
2022-10-09 16:41:46 -0700

    make it column not grid

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,7 @@
 import React from "react";
 import styles from "./App.module.css";
 import { Badge } from "./components/badge/badge";
-import { IPadCursor } from "./components/ipad-cursor/ipad-cursor";
+// import { IPadCursor } from "./components/ipad-cursor/ipad-cursor";
 import { Marquee } from "./components/marquee/marquee";
 import { SnapScrolling } from "./components/snap-scrolling/snap-scrolling";
 import { Zoom } from "./components/zoom/zoom";
@@ -16,21 +16,21 @@ function App() {
     { name: "Marquee", node: <Marquee /> },
     { name: "Snap Scrolling", node: <SnapScrolling /> },
     { name: "Zoom", node: <Zoom /> },
-    { name: "IPad Cursor", node: <IPadCursor /> },
+    // { name: "IPad Cursor", node: <IPadCursor /> },
   ];
 
   return (
     <div className={styles.root}>
-      <div className={styles.grid}>
-        {components.map(({ name, node }, index) => (
-          <div className={styles.gridItem}>
-            <Badge className={styles.badge} index={index + 1}>
-              {name}
-            </Badge>
-            {node}
-          </div>
-        ))}
-      </div>
+      {/* <div className={styles.grid}> */}
+      {components.map(({ name, node }, index) => (
+        <div className={styles.item}>
+          <Badge className={styles.badge} index={index + 1}>
+            {name}
+          </Badge>
+          {node}
+        </div>
+      ))}
+      {/* </div> */}
     </div>
   );
 }
