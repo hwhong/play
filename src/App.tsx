383c18551d324bc62afe34c51cfa62e6b96e0b68
Warren Hong <hwhong1129@gmail.com>
2022-12-27 21:41:40 -0800

    feature: redid the layout with google mui. added nike global, wip though.

--- src/App.tsx
+++ src/App.tsx
@@ -12,6 +12,11 @@ import { ScrollListener } from "./components/scroll-listener/scroll-listener";
 import { Accordion } from "./components/accordion/accordion";
 import { InfiniteCircle } from "./components/infinite-circle/infinite-circle";
 import { Button } from "./components/button/button";
+import { NikeGlobal } from "./components/nike-global/nike-global";
+import Tabs from "@mui/material/Tabs";
+import Tab from "@mui/material/Tab";
+import Typography from "@mui/material/Typography";
+import Box from "@mui/material/Box";
 // import { Parallax } from "./components/parallax/parallax";
 
 interface Content {
@@ -19,7 +24,36 @@ interface Content {
   node: React.ReactNode;
 }
 
+interface TabPanelProps {
+  children?: React.ReactNode;
+  index: number;
+  value: number;
+}
+
+function TabPanel(props: TabPanelProps) {
+  const { children, value, index, ...other } = props;
+
+  return (
+    <div
+      role="tabpanel"
+      hidden={value !== index}
+      id={`simple-tabpanel-${index}`}
+      aria-labelledby={`simple-tab-${index}`}
+      {...other}
+      className={styles.tabPanel}
+    >
+      {value === index && children}
+    </div>
+  );
+}
+
 function App() {
+  const [value, setValue] = React.useState(0);
+
+  const handleChange = (event: React.SyntheticEvent, newValue: number) => {
+    setValue(newValue);
+  };
+
   const components: Content[] = [
     { name: "Marquee", node: <Marquee /> },
     { name: "Snap Scrolling", node: <SnapScrolling /> },
@@ -47,22 +81,41 @@ function App() {
       name: "Button",
       node: <Button onClick={() => console.log("Blah")} children={1} />,
     },
+    {
+      name: "Nike Global",
+      node: <NikeGlobal />,
+    },
   ];
 
   return (
     <div className={styles.root}>
-      {components.map(({ name, node }, index) => (
-        <div
-          className={classNames(styles.item, {
-            [styles.off]: name === "Slider",
-          })}
+      <Box
+        sx={{
+          flexGrow: 1,
+          bgcolor: "background.paper",
+          display: "flex",
+          height: "100vh",
+        }}
+      >
+        <Tabs
+          orientation="vertical"
+          variant="scrollable"
+          value={value}
+          onChange={handleChange}
+          aria-label="Vertical tabs example"
+          sx={{ borderRight: 1, borderColor: "divider" }}
         >
-          <Badge className={styles.badge} index={index + 1}>
-            {name}
-          </Badge>
-          {node}
-        </div>
-      ))}
+          {components.map(({ name }) => (
+            <Tab label={name} />
+          ))}
+        </Tabs>
+
+        {components.map(({ node }, i) => (
+          <TabPanel value={value} index={i}>
+            {node}
+          </TabPanel>
+        ))}
+      </Box>
     </div>
   );
 }
---
+++ src/components/nike-global/nike-global.tsx
@@ -0,0 +1,82 @@
+import { motion, useAnimation } from "framer-motion";
+import React from "react";
+import styles from "./nike-global.module.css";
+
+export function NikeGlobal() {
+  const buttonControl = useAnimation();
+  const originControl = useAnimation();
+  const languageControl = useAnimation();
+
+  async function animateStartSequence() {
+    await originControl.start({
+      display: "none",
+    });
+    await buttonControl.start({
+      height: 100,
+      transition: { duration: 0.3 },
+    });
+    await buttonControl.start({ width: 130, transition: { duration: 0.3 } });
+    return await languageControl.start({
+      display: "inline-block",
+      opacity: 1,
+      transition: { duration: 0.3 },
+    });
+  }
+
+  async function animateEndSequence() {
+    await languageControl.start({
+      display: "none",
+    });
+    await buttonControl.start({ width: 162, transition: { duration: 0.3 } });
+    await buttonControl.start({
+      height: 40,
+      transition: { duration: 0.3 },
+    });
+    await originControl.start({
+      display: "block",
+    });
+    // something weird is happening here
+    await languageControl.set({
+      display: "none",
+    });
+    await buttonControl.set({ width: 162 });
+  }
+
+  return (
+    <div className={styles.root}>
+      <div className={styles.wrapper}>
+        <motion.div
+          initial={{
+            height: 40,
+            width: 162,
+          }}
+          className={styles.control}
+          animate={buttonControl}
+          onClick={() => animateStartSequence()}
+        >
+          <motion.div animate={originControl} className={styles.origin}>
+            Canada
+          </motion.div>
+          <motion.ul
+            className={styles.list}
+            animate={languageControl}
+            initial={{ display: "none", opacity: 0 }}
+          >
+            <motion.li
+              className={styles.listItem}
+              onClick={() => animateEndSequence()}
+            >
+              English
+            </motion.li>
+            <motion.li
+              className={styles.listItem}
+              onClick={() => animateEndSequence()}
+            >
+              Français
+            </motion.li>
+          </motion.ul>
+        </motion.div>
+      </div>
+    </div>
+  );
+}
